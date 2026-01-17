633b51f570893c8b76b9d587fa8427987e52d892
Miguel Salvador <miguel_f_r_salvador@hotmail.com>
2026-01-17 17:44:09 +0000

    fix: removed yarn.lock to change from yarn build

--- src/routes/signUp.tsx
+++ src/routes/signUp.tsx
@@ -31,12 +31,13 @@ export function SignUp() {
         <TextInput label="Username" placeholder="Enter username" required radius="md" />
         <PasswordInput label="Password" placeholder="Your password" required radius="md" />
         <TextInput label="Email" placeholder="you@email.com" required radius="md" />
+            <a
+    href="{{ .SiteURL }}/auth/confirm?token_hash={{ .TokenHash }}&type=email&next={{ .RedirectTo }}">
         <Button fullWidth mt="xl" radius="md">
           Sign Up
-            <a
-    href="{{ .SiteURL }}/auth/confirm?token_hash={{ .TokenHash }}&type=email&next={{ .RedirectTo }}"
-    >Confirm your email</a>
+    Confirm your email
         </Button>
+        </a>
       </Paper>
     </Container>
   );
